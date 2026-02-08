9c0b0c5d3feaa4e61b37bfc2604f105b803850f2
Staley <staleylane101@gmail.com>
2026-02-08 14:06:29 +0800

    Calculator class
    Mao ni ang calculator class.

--- Calculator.cs
+++ Calculator.cs
@@ -4,7 +4,33 @@ public static class Calculator
 {
     public static double? Calculate(int first, int second, string operation)
     {
-        //write your business logic in here
+         switch (operation)
+        {
+            case "+":
+                return first + second;
+
+            case "-":
+                return first - second;
+
+            case "*":
+                return first * second;
+
+            case "/":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    return null;
+                }
+                return (double)first / second;
+
+            case "%":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot modulo by zero");
+                    return null;
+                }
+                return first % second;
+        }
 
         return null;
     }
